2accc8f734aefd445194d703c83139ef819ec878
freygh1 <freilinshernandez10@gmail.com>
2025-07-28 01:49:05 -0400

    Se han creado las funciones: limpiarInput y seleccionDeEscala

--- script.js
+++ script.js
@@ -5,6 +5,32 @@ const toSelector = document.getElementById('to_selector')
 const inputValue = document.getElementById('input_value')
 const convertBtn = document.getElementById('convert_btn')
 const resultCtn = document.getElementById('result_ctn')
-const resetBtn = document.getElementById('reset_btn')
+const resetBtn = document.getElementById('reset_btn') 
+
+        
+const limpiarInput = () => inputValue.value = ''
+
+const seleccionDeEscala = () =>{
+   fromSelector.addEventListener('change', (seleccion) =>{
+        limpiarInput()  
+        switch (seleccion.target.value) {
+        case "kelvin":
+        inputValue.min = "0" 
+        break;        
+        case "celsius":
+        inputValue.min = "-273.15"
+        break;
+        case "fahrenheit":
+        inputValue.min = "-459.67"
+        break
+    }
+  })
+}
+
+seleccionDeEscala()
+
+// function verificarValidezDelValor() {
+  
+// }
 
 
