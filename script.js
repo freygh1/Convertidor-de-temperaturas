1a36723feedcb13997bde8f1a952e4293e47a64e
freygh1 <freilinshernandez10@gmail.com>
2025-07-27 21:50:46 -0400

    se ha accedido a los elementos HTML necesarios

---
+++ script.js
@@ -0,0 +1,10 @@
+// Accedemos a nuestros elementos:
+
+const fromSelector = document.getElementById('from_selector')
+const toSelector = document.getElementById('to_selector')
+const inputValue = document.getElementById('input_value')
+const convertBtn = document.getElementById('convert_btn')
+const resultCtn = document.getElementById('result_ctn')
+const resetBtn = document.getElementById('reset_btn')
+
+
