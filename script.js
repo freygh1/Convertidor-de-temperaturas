cad60a9a90cb490e1e08fbff21025892f7d3b62a
freygh1 <freilinshernandez10@gmail.com>
2025-07-29 01:17:00 -0400

    Completada la funcion de calculo de conversion entre  las escalas temperaturas F, K, C

--- script.js
+++ script.js
@@ -11,9 +11,13 @@ const limpiarInput = () => inputValue.value = ''
 
 const cambiarPlaceholder = () => {
       if (fromSelector.value === toSelector.value) {
+            inputValue.setAttribute('disabled', 'true')
+            convertBtn.setAttribute('disabled', 'true')
             inputValue.setAttribute('placeholder', 'Las escalas no pueden ser iguales')
       }
       else {
+            convertBtn.removeAttribute('disabled')
+            inputValue.removeAttribute('disabled')
             inputValue.setAttribute('placeholder', 'Digita valor a convertir...')
       }
 }
@@ -50,8 +54,38 @@ const seleccionDeEscala = () => {
 
 seleccionDeEscala()
 
-// const verificarValidezDelValor = () => {
+const calculosEntreEscalas = (num, from, to) => {
+      let resultados;
+      num = parseFloat(num)
+      // Se convertira a celsius primero
+      let celsius;
+      if (from === 'kelvin') {
+            celsius = num - 273.15
+      } else if (from === 'fahrenheit') {
+            celsius = (num - 32) * (5 / 9)
+      } else {
+            celsius = num
+      }
 
-// }
+      // Ahora, la conversion final
+
+      if (to === 'kelvin') {
+            resultados = celsius + 273.15
+      } else if (to === 'fahrenheit') {
+            resultados = celsius * (9 / 5) + 32
+      } else {
+            resultados = celsius
+      }
+
+      return `${resultados.toFixed(2)} °${toSelector.value}`
+}
+
+convertBtn.addEventListener('click', () => {
+      resultCtn.textContent = calculosEntreEscalas(
+            inputValue.value,
+            fromSelector.value,
+            toSelector.value
+      )
+})
 
 
